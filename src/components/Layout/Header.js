9cf702d1d9c22d0d7f50b02a42295a8228041bce
Jingwen Li <jingwen.li@verkada.com>
2022-07-10 16:08:52 -0700

    added header and image

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import mealImage from "../../assets/meals.jpeg"
+import classes from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <React.Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealImage} alt='A table full of delicious dishes.'/>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Header
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from 'HeaderCarButton.module.css';
+import CartIcon from ''
+
+const HeaderCartButton = () => {
+  return (
+    <div>HeaderCartButton</div>
+  )
+}
+
+export default HeaderCartButton
