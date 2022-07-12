0a3c9bec23ed425ff4a32e7a04f7129521c6b302
JingleOne <jingwenli0851@gmail.com>
2022-07-12 09:24:42 -0700

    added food summary and list of fake food data

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import mealImage from "../../assets/meals.jpeg"
 import classes from "./Header.module.css"
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = () => {
   return (
     <React.Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealImage} alt='A table full of delicious dishes.'/>
