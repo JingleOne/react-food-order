600d16815b702473dabb736137a0df586a0e87b0
JingleOne <jingwenli0851@gmail.com>
2022-08-05 00:47:34 -0700

    Added a reducer action to handle add to cart

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import classes from './MealItem.module.css';
-import MealItemForm from './MealItemForm';
+import React, { useContext } from "react";
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../Store/cart-context";
 
 const MealItem = (props) => {
-
+  const cartCtx = useContext(CartContext);
   const price = props.price.toFixed(2);
+
+  const handleAddToCart = (itemAmount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      price: price,
+      amount: itemAmount,
+    });
+  };
+
   return (
     <li className={classes.meal}>
-        <div className={classes.mealItem}>
-          <h3>{props.name}</h3>
-          <div className={classes.description}>
-            {props.description}
-          </div>
-          <div className={classes.price}>
-            {price}
-          </div>
-        </div>
-        <div>
-          <MealItemForm id={props.id}/>
-        </div>
+      <div className={classes.mealItem}>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} onAddCart={handleAddToCart} />
+      </div>
     </li>
   );
-}
+};
 
 export default MealItem;
